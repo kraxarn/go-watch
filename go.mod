80fe2624f0653a4f2a0877b7c9a479a66d094038
kraxarn <me@kraxarn.com>
2021-02-13 19:09:24 +0100

    Some updated routing

--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module github.com/kraxarn/go-watch
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.6.3
-	github.com/kraxarn/website v0.0.0-20210125171941-95b46297ef6d
+	github.com/kraxarn/website v0.0.0-20210211220306-9c1e47c15390
 )
---
+++ main.go
@@ -0,0 +1,58 @@
+package watch
+
+import (
+	"fmt"
+	"github.com/kraxarn/website/config"
+	"github.com/kraxarn/website/user"
+	"html/template"
+	"net/http"
+)
+
+type Model struct {
+	CurrentUser *user.User
+	Avatars     []user.Avatar
+	Room        string
+}
+
+func Route(token *config.Token) {
+	// Main state handler
+	watch := Watch{
+		token: token,
+	}
+
+	// Static files
+	for _, path := range []string{
+		"css", "img", "js",
+	} {
+		http.Handle(path, http.FileServer(http.Dir(fmt.Sprintf("static/%s", path))))
+	}
+
+	templates, err := template.ParseGlob("html/*.gohtml")
+	if err != nil {
+		fmt.Printf("failed to parse templates: %v\n", err)
+		return
+	}
+
+	http.HandleFunc("/watch", func(writer http.ResponseWriter, request *http.Request) {
+		currentUser := watch.getUser(writer, request)
+		err := templates.Lookup("watch.gohtml").Execute(writer, Model{
+			CurrentUser: currentUser,
+			Avatars:     user.AvatarValues,
+		})
+		if err != nil {
+			fmt.Printf("/watch filed: %v\n", err)
+		}
+	})
+
+	http.HandleFunc("/watch/room/:id", func(writer http.ResponseWriter, request *http.Request) {
+		currentUser := watch.getUser(writer, request)
+		err := templates.Lookup("room.gohtml").Execute(writer, Model{
+			CurrentUser: currentUser,
+			Avatars:     user.AvatarValues,
+			Room:        request.URL.Query().Get("id"),
+		})
+		if err != nil {
+			fmt.Printf("/watch/room filed: %v\n", err)
+		}
+	})
+}
